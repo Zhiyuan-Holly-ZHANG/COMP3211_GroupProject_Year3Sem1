8a1ec41a3f216532f4af10f0c124e80436d9e077
黄添骥 <2250016230@qq.com>
2023-11-23 19:29:35 +0800

    change directory of main of test

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-from Test_Contacts import Test_Contacts
-from Test_Search import Test_Search
-from Test_Notes import Test_Notes
-from Test_Events import Test_Events
-from Test_Item import TestItem
-from Test_Tasks import Test_Tasks
-from Test_Alarm import Test_Alarm
+from Test.Test_Contacts import Test_Contacts
+from Test.Test_Search import Test_Search
+from Test.Test_Notes import Test_Notes
+from Test.Test_Events import Test_Events
+from Test.Test_Item import TestItem
+from Test.Test_Tasks import Test_Tasks
+from Test.Test_Alarm import Test_Alarm
 import unittest
 
 
